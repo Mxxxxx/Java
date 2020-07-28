6c368a9ac37a600b05c31c1db390d08cf13de3d8
孟鑫 <907008127@qq.com>
2020-07-28 22:10:30 +0800

    tj

---
+++ Java/src/OJ题/插入排序.java
@@ -0,0 +1,24 @@
+package OJ题;
+
+/**
+ * @Author Meng Xin
+ * @Date 2020/7/28 21:33
+ */
+public class 插入排序 {
+    public static void func(int[] array) {
+        int tmp = array[0];
+        for (int i = 1; i < array.length; i++) {
+            tmp = array[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (array[j] > tmp) {//i的前一个比i大的话，将大的值赋值给i
+                    array[j + 1] = array[j];
+                } else {
+//                    array[j + 1] = tmp;
+                    break;//如果i的前一个比i小，前面的就不需要再比较，直接跳出
+                }
+            }
+            array[j + 1] = tmp;
+        }
+    }
+}
