53d95786ebb3f0b2d7f7029efda20054f04b0724
孟鑫 <907008127@qq.com>
2020-08-06 21:16:28 +0800

    tj

--- Java/src/OJ题/线程例子.java
+++ Java/src/OJ题/线程例子.java
@@ -1,5 +1,7 @@
 package OJ题;
 
+import java.util.concurrent.*;
+
 /**
  * @Author Meng Xin
  * @Date 2020/8/5 22:21
@@ -15,6 +17,19 @@ public class 线程例子 {
     public static void main(String[] args) {
         MyThreag t = new MyThreag();
         t.start();
-        
+
+    }
+
+    //线程池
+    public void threadPoolExecutor(
+            int corePoolSize,//线程池的核心大小
+            int maximumPoolSize,//最大线程池大小。
+            long keepAliveTime,//空余线程存活时间
+            TimeUnit unit,//销毁时间单位
+            BlockingQueue<Runnable> workQueue,//存储等待执行线程的工作队列
+            ThreadFactory threadFactory,//创建线程的工厂
+            RejectedExecutionHandler handler//拒绝策略，当工作队列、线程池全已满时如何拒绝新任务，默认抛出异常。
+    ) {
+        return;
     }
 }
