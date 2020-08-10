fe0d83037a81eb8c567d314545a7caf7e11cdba1
孟鑫 <907008127@qq.com>
2020-08-11 07:35:55 +0800

    tj

--- Java/src/OJ题/归并排序.java
+++ Java/src/OJ题/归并排序.java
@@ -8,6 +8,44 @@ package OJ题;
 //空间:O(n)
 //稳定
 public class 归并排序 {
+    public static void mergeSort1(int[] array) {
+        mereSorInt1(array, 0, array.length - 1);
+    }
+
+    private static void mereSorInt1(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (high + low) >>> 1;
+        mereSorInt1(array, low, mid);
+        mereSorInt1(array, mid + 1, high);
+        merge1(array, low, mid, high);
+    }
+
+    private static void merge1(int[] array, int low, int mid, int high) {
+        int s1 = low;
+        int s2 = mid + 1;
+        int length = high - low + 1;
+        int[] ret = new int[length];
+        int i = 0;
+        while (s1 <= mid && s2 <= high) {
+            if (array[s1] <= array[s2]) {
+                ret[i++] = array[s1++];
+            } else {
+                ret[i++] = array[s2++];
+            }
+        }
+        while (s1 <= mid) {
+            ret[i++] = array[s1++];
+        }
+        while (s2 <= high) {
+            ret[i++] = array[s2++];
+        }
+        for (int j = 0; j < ret.length; j++) {
+            array[j + low] = ret[j];
+        }
+    }
+
     public static void mereSorInt(int[] array, int low, int high) {
         if (low >= high) {
             return;
