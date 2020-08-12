6c014a1e6e0909b1b83730263eebd6bd59a46054
孟鑫 <907008127@qq.com>
2020-08-12 16:36:39 +0800

    tj

--- Java/src/OJ题/归并排序.java
+++ Java/src/OJ题/归并排序.java
@@ -9,43 +9,81 @@ package OJ题;
 //稳定
 public class 归并排序 {
     public static void mergeSort1(int[] array) {
-        mereSorInt1(array, 0, array.length - 1);
+        mereSorInit1(array, 0, array.length - 1);
     }
 
-    private static void mereSorInt1(int[] array, int low, int high) {
-        if (low >= high) {
+    private static void mereSorInit1(int[] array, int low, int hight) {
+        if (low >= hight) {
             return;
         }
-        int mid = (high + low) >>> 1;
-        mereSorInt1(array, low, mid);
-        mereSorInt1(array, mid + 1, high);
-        merge1(array, low, mid, high);
+        int mid = (low + hight) >>> 1;
+        mereSorInit1(array, low, mid);
+        mereSorInit1(array, mid + 1, hight);
+        merge1(array, low, mid, hight);
     }
 
-    private static void merge1(int[] array, int low, int mid, int high) {
+    private static void merge1(int[] array, int low, int mid, int hight) {
         int s1 = low;
         int s2 = mid + 1;
-        int length = high - low + 1;
-        int[] ret = new int[length];
+        int len = hight - low + 1;
+        int[] ret = new int[len];
         int i = 0;
-        while (s1 <= mid && s2 <= high) {
+        while (s1 <= mid && s2 <= hight) {
             if (array[s1] <= array[s2]) {
                 ret[i++] = array[s1++];
             } else {
                 ret[i++] = array[s2++];
             }
         }
-
         while (s1 <= mid) {
             ret[i++] = array[s1++];
         }
-        while (s2 <= high) {
+        while (s2 <= hight) {
             ret[i++] = array[s2++];
         }
         for (int j = 0; j < ret.length; j++) {
             array[j + low] = ret[j];
         }
     }
+
+//    public static void mergeSort1(int[] array) {
+//        mereSorInt1(array, 0, array.length - 1);
+//    }
+//
+//    private static void mereSorInt1(int[] array, int low, int high) {
+//        if (low >= high) {
+//            return;
+//        }
+//        int mid = (high + low) >>> 1;
+//        mereSorInt1(array, low, mid);
+//        mereSorInt1(array, mid + 1, high);
+//        merge1(array, low, mid, high);
+//    }
+//
+//    private static void merge1(int[] array, int low, int mid, int high) {
+//        int s1 = low;
+//        int s2 = mid + 1;
+//        int length = high - low + 1;
+//        int[] ret = new int[length];
+//        int i = 0;
+//        while (s1 <= mid && s2 <= high) {
+//            if (array[s1] <= array[s2]) {
+//                ret[i++] = array[s1++];
+//            } else {
+//                ret[i++] = array[s2++];
+//            }
+//        }
+//
+//        while (s1 <= mid) {
+//            ret[i++] = array[s1++];
+//        }
+//        while (s2 <= high) {
+//            ret[i++] = array[s2++];
+//        }
+//        for (int j = 0; j < ret.length; j++) {
+//            array[j + low] = ret[j];
+//        }
+//    }
 
     public static void mereSorInt(int[] array, int low, int high) {
         if (low >= high) {
