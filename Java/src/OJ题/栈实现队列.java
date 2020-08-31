c325c7e7c3053e0b0a44adf3f4c123df0007f32a
孟鑫 <907008127@qq.com>
2020-08-31 09:48:53 +0800

    tj

---
+++ Java/src/OJ题/栈实现队列.java
@@ -0,0 +1,25 @@
+package OJ题;
+
+import java.util.Stack;
+
+/**
+ * @Author Meng Xin
+ * @Date 2020/8/31 9:43
+ */
+public class 栈实现队列 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+    public int pop() {
+        if (stack2.size() <= 0) {
+            while (stack1.size() != 0) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
+
